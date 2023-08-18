16ec4b09f1bdaea4f83ffafc9b0ba8f0ff40c627
unknown <mabrukamustafam@gmail.com>
2023-08-18 19:10:53 +0300

    answered

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Good Luck 💪🏾
+
+function count(stringone,stringtwo) {
+    let count=0
+    for (let i = 0; i < stringtwo.length; i++) {
+        if (stringtwo[i] === stringone) {
+            count++;
+        }
+
+        
+    }
+    return count;
+    
+}
+
+  console.log(count("a", "fikrcamp")); 
+  console.log(count("c", "bootcamp")); 
+  console.log(count("o", "fikrcamp coding bootcamp")); 
+
+
